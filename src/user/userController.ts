4957148423f588bea535ff16d2611514a90c06e2
tanwarabhinandan <tanwar.abhinandan@gmail.com>
2024-06-07 19:22:03 +0530

    add error handling

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -4,6 +4,7 @@ import userModel from "./userModel";
 import bcrypt from "bcrypt";
 import { sign } from "jsonwebtoken";
 import { config } from "../config/config";
+import { User } from "./userTypes";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -14,33 +15,49 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
   }
 
   // Database call
-  const user = await userModel.findOne({ email });
-
-  if (user) {
-    const error = createHttpError(400, "User Already Exist with this Email ID");
-    return next(error);
+  try {
+    const user = await userModel.findOne({ email });
+    if (user) {
+      const error = createHttpError(
+        400,
+        "User Already Exist with this Email ID"
+      );
+      return next(error);
+    }
+  } catch (err) {
+    return createHttpError(500, "Error while finding user on Database");
   }
 
-  // Password - Hashed
-  const hashedPassword = await bcrypt.hash(password, 10);
+  let newUser: User;
+  try {
+    // Password - Hashed
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-  const newUser = await userModel.create({
-    name,
-    email,
-    password: hashedPassword,
-  });
+    newUser = await userModel.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+  } catch (err) {
+    return createHttpError(500, "Error While Hashing Password");
+  }
 
-  // token generation - JWT
+  let token: string;
+  try {
+    // token generation - JWT
 
-  const token = sign({ sub: newUser._id }, config.jwtSecret as string, {
-    expiresIn: "7d",
-    algorithm: 'HS256'
-  });
+    token = sign({ sub: newUser._id }, config.jwtSecret as string, {
+      expiresIn: "7d",
+      algorithm: "HS256",
+    });
+  } catch (err) {
+    return createHttpError(500, "Error While Hashing Password");
+  }
 
   //Logic - Process
 
   //Response
-  res.json({
+  res.status(201).json({
     message: "User Created..!!!",
     id: newUser._id,
     accessToken: token,

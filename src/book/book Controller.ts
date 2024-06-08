be64d4677941530f0960c22dfe1e4e2b88b3c2b7
tanwarabhinandan <tanwar.abhinandan@gmail.com>
2024-06-09 02:20:29 +0530

    Update book

--- src/book/book Controller.ts
+++ src/book/book Controller.ts
@@ -6,6 +6,7 @@ import bookModel from "./bookModel";
 import fs from "node:fs";
 import { Authrequest } from "../middlewares/authenticate";
 
+// Create Book
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
   const { title, genre } = req.body;
 
@@ -25,7 +26,7 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
       imageFileName
     );
     // console.log(imageFileName);
-    // cloudinary File upload
+    // cloudinary Image File upload
     const imageFileUploadResult = await cloudinary.uploader.upload(
       imageFilePath,
       {
@@ -90,4 +91,104 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-export { createBook };
+// Book Update
+const updateBook = async (req: Request, res: Response, next: NextFunction) => {
+  const { title, genre } = req.body;
+  const bookId = req.params.bookId;
+
+  const book = await bookModel.findOne({ _id: bookId });
+
+  if (!book) {
+    return next(createHttpError(404, "Book Not Found"));
+  }
+
+  // Check Access
+  const _req = req as Authrequest;
+  if (book.author.toString() !== _req.userId) {
+    return next(createHttpError(404, "Not authorized for Update"));
+  }
+
+  // to check - image and pdf (new / updated)
+
+  // to get file information from req obj
+  const files = req.files as { [fileName: string]: Express.Multer.File[] };
+
+  // to check Image file is already exist
+  let completeCoverImage = "";
+  if (files.coverImage) {
+    const coverImageMimeType = files.coverImage[0].mimetype.split("/").at(-1);
+    const imageFileName = files.coverImage[0].filename;
+    const imageFilePath = path.resolve(
+      __dirname,
+      "../../public/data/uploads",
+      imageFileName
+    );
+
+    completeCoverImage = imageFileName;
+
+    // cloudinary Image File upload
+    const imageFileUploadResult = await cloudinary.uploader.upload(
+      imageFilePath,
+      {
+        filename_override: imageFileName,
+        folder: "book-covers",
+        format: coverImageMimeType,
+      }
+    );
+
+    completeCoverImage = imageFileUploadResult.secure_url;
+    await fs.promises.unlink(imageFilePath);
+  }
+
+  // to check PDF file is already exist
+  let completeFile = "";
+  if (files.file) {
+    const bookMimeType = files.file[0].mimetype.split("/").at(-1);
+    const bookFileName = files.file[0].filename;
+    const bookFilePath = path.resolve(
+      __dirname,
+      "../../public/data/uploads",
+      bookFileName
+    );
+
+    const bookFile = bookFileName;
+    completeFile = bookFile;
+
+    // Cloudinary PDF file upload
+    const bookFileUploadResult = await cloudinary.uploader.upload(
+      bookFilePath,
+      {
+        resource_type: "raw",
+        filename_override: bookFileName,
+        folder: "book-pdfs",
+        format: bookMimeType,
+      }
+    );
+
+    completeFile = bookFileUploadResult.secure_url;
+    await fs.promises.unlink(bookFilePath);
+  }
+
+  // to update book record in Database
+  const updatedBook = await bookModel.findOneAndUpdate(
+    {
+      _id: bookId,
+    },
+    {
+      title: title,
+      genre: genre,
+      coverImage: completeCoverImage ? completeCoverImage : book.coverImage,
+      file: completeFile ? completeFile : book.file,
+    },
+    {
+      new: true,
+    }
+  );
+
+  res.json({
+    message: "Book Updated",
+    updatedBook,
+  });
+};
+
+export { createBook, updateBook };
--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { createBook } from "./book Controller";
+import { createBook, updateBook } from "./book Controller";
 import multer from "multer";
 import path from "node:path";
 import authenticate from "../middlewares/authenticate";
@@ -23,6 +23,16 @@ bookRouter.post(
     { name: "file", maxCount: 1 },
   ]),
   createBook
+);
+
+bookRouter.patch(
+  "/:bookId",
+  authenticate,
+  upload.fields([
+    { name: "coverImage", maxCount: 1 },
+    { name: "file", maxCount: 1 },
+  ]),
+  updateBook
 );
 
 export default bookRouter;
